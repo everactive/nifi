12697bf085c66fc88fe627a8b59628f72cbadfee
Carlos Olmos <carlos.olmos@psikick.com>
2020-09-16 10:07:39 -0700

    my build script

---
+++ mybuild.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export MAVEN_OPTS="-Xms1024m -Xmx3076m -XX:MaxPermSize=256m"
+if [[ "$1" == "rebuild" ]]; then
+mvn -T C2.0 clean install -Pinclude-grpc -DskipTests
+fi
+
+if [[ "$1" == "assembly" ]]; then
+  cd nifi-assembly
+  mvn -T C2.0 clean install -Ddir-only
+  cd -
+fi
+
+if [[ "$1" == "amqp" ]]; then
+  cd nifi-nar-bundles/nifi-amqp-bundle
+  mvn -T C2.0 clean install -Pinclude-grpc
+  cd -
+  cp nifi-nar-bundles/nifi-amqp-bundle/nifi-amqp-nar/target/nifi-amqp-nar-1.13.0-SNAPSHOT.nar ./nifi-amqp-nar-1.13.0-everactive.nar
+fi
+
